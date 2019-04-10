7004adab89d687744e111816d39ebc02cf8eadba
Luke Larson <luke.larson@gmail.com>
2019-04-10 16:27:33 -0700

    fixed all old schema internals

---
+++ Assets/PiRhoSoft Composition/Scripts/Engine/Behaviours/GraphTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace PiRhoSoft.CompositionEngine
+{
+	[HelpURL(Composition.DocumentationUrl + "graph-trigger")]
+	[AddComponentMenu("PiRho Soft/Composition/Graph Trigger")]
+	public class GraphTrigger : MonoBehaviour
+	{
+		[Tooltip("The graph to run when this object is triggered")]
+		public InstructionCaller Graph = new InstructionCaller();
+
+		public void Run()
+		{
+			if (Graph.Instruction)
+				CompositionManager.Instance.RunInstruction(Graph, VariableValue.Create(this));
+		}
+	}
+}
