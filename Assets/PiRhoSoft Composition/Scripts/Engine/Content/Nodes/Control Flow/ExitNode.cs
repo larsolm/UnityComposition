0d5ec9ddfa7a9e7040841004c78ba19daa1222f3
Adam Larson <larsoad@gmail.com>
2019-05-06 18:17:06 -0700

    exit node

---
+++ Assets/PiRhoSoft Composition/Scripts/Engine/Content/Nodes/Control Flow/ExitNode.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace PiRhoSoft.CompositionEngine
+{
+	[CreateInstructionGraphNodeMenu("Control Flow/Exit", 23)]
+	[HelpURL(Composition.DocumentationUrl + "exit-node")]
+	public class ExitNode : InstructionGraphNode
+	{
+		public override Color NodeColor => Colors.Break;
+
+		public override IEnumerator Run(InstructionGraph graph, InstructionStore variables, int iteration)
+		{
+			graph.BreakAll();
+			yield break;
+		}
+	}
+}
